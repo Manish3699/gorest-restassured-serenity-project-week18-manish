fa64157d2746388544cd7c9a1191a375da97e581
Manish3699 <uniquelaure07@gamail.com>
2025-06-09 22:38:39 +0100

    My homework week 18

---
+++ src/test/java/com/gorest/info/UserCRUDTest.java
@@ -0,0 +1,65 @@
+package com.gorest.info;
+
+import com.gorest.testbase.TestBase;
+import com.gorest.utils.TestUtils;
+import io.restassured.response.ValidatableResponse;
+import net.serenitybdd.annotations.Steps;
+import net.serenitybdd.annotations.Title;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.HashMap;
+
+import static org.hamcrest.Matchers.hasValue;
+
+@RunWith(SerenityRunner.class)
+public class UserCRUDTest extends TestBase {
+
+    static int usersId;
+    static String name = TestUtils.getRandomValue() + "RamKumar";
+    static String email = TestUtils.getRandomValue() + "ramkuma123@gmail.com";
+    static String gender = "male";
+    static String status = "active";
+
+    @Steps
+    UserSteps steps;
+
+    @Title("This will create the user")
+    @Test
+    public void Test1() {
+
+        ValidatableResponse response = steps.createUser(name, email, gender, status);
+        response.log().all().statusCode(201);
+        usersId = response.log().all().extract().path("id");
+        System.out.println(usersId);
+
+    }
+
+    @Title("This will read the user")
+    @Test
+    public void Test2() {
+        HashMap<String, Object> userMap = steps.readUser(usersId);
+        Assert.assertThat(userMap, hasValue(name));
+    }
+
+    @Title("This will update the user")
+    @Test
+    public void Test3() {
+        name = "Avi" + TestUtils.getRandomValue();
+        email = "avi@gmail.com" + TestUtils.getRandomValue();
+        steps.updateUser(usersId, name, email, gender, status).statusCode(200);
+
+        HashMap<String, Object> userMap = steps.readUser(usersId);
+        Assert.assertThat(userMap, hasValue(usersId));
+
+    }
+
+    @Title("This will delete the user")
+    @Test
+    public void Test4() {
+        steps.deleteUser(usersId).statusCode(204);
+        steps.getUserById(usersId).statusCode(404);
+    }
+}
